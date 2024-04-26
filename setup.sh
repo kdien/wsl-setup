2da737a4543ca468288215cd6ff7a2a154ecf3dc
Khoa Dien <khoa.dien@hotmail.com>
2024-04-26 10:46:43 -0300

    Fix syntax issue with adding Docker repo

--- setup.sh
+++ setup.sh
@@ -47,9 +47,10 @@ sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+# shellcheck source=/dev/null
 sudo tee /etc/apt/sources.list.d/docker.list >/dev/null <<EOF
-"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-$(. /etc/os-release && echo "$VERSION_CODENAME") stable"
+deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable
 EOF
 sudo apt update
 
