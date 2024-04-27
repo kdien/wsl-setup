ce7b20df08e16fa6bcfeacb661c41454ea6a9670
Khoa Dien <khoa.dien@hotmail.com>
2024-04-27 15:29:41 -0300

    Add step to install GitHub CLI

--- setup.sh
+++ setup.sh
@@ -73,10 +73,18 @@ sudo apt remove -y docker.io docker-doc docker-compose docker-compose-v2 podman-
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-# shellcheck source=/dev/null
 sudo tee /etc/apt/sources.list.d/docker.list >/dev/null <<EOF
 deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
 $(. /etc/os-release && echo "$VERSION_CODENAME") stable
 EOF
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add GitHub repo and install `gh`
+sudo curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg -o /etc/apt/keyrings/github-cli.gpg
+sudo chmod a+r /etc/apt/keyrings/github-cli.gpg
+sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null <<EOF
+deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/github-cli.gpg] https://cli.github.com/packages stable main
+EOF
+sudo apt update
+sudo apt install -y gh
